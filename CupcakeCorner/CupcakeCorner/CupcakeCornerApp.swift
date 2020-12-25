20db748462a6eef623345194c9169687b9bbe585
Dave Spina <dspina79@gmail.com>
2020-12-25 01:42:27 -0500

    Initial app creation

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Dave Spina on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
