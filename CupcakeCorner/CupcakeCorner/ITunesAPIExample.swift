c0a0a5c5ae89ff051cf3989caa480d503cabe157
Dave Spina <dspina79@gmail.com>
2020-12-26 21:25:54 -0500

    Example of strongly typed codable

---
+++ CupcakeCorner/CupcakeCorner/ITunesAPIExample.swift
@@ -0,0 +1,20 @@
+//
+//  ITunesAPIExample.swift
+//  CupcakeCorner
+//
+//  Created by Dave Spina on 12/26/20.
+//
+
+import SwiftUI
+
+struct ITunesAPIExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ITunesAPIExample_Previews: PreviewProvider {
+    static var previews: some View {
+        ITunesAPIExample()
+    }
+}
