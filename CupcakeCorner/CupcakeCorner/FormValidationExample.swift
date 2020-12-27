c1fc537ed75c5122519da226ab2bf6569f768a46
Dave Spina <dspina79@gmail.com>
2020-12-26 21:51:20 -0500

    Completed validation example form

--- CupcakeCorner/CupcakeCorner/FormValidationExample.swift
+++ CupcakeCorner/CupcakeCorner/FormValidationExample.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct FormValidationExample: View {
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableButton: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            Section {
+                Button("Create Account") {
+                    print("Creating Account...")
+                }
+            }.disabled(disableButton)
+        }
     }
 }
 
